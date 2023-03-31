c7fd1c71f86fc397501c1109f70a32031f963744
bakibillah <bakibillah.dev@gmail.com>
2023-03-31 16:47:53 +0600

    methods written

--- projectMediLog/src/main/java/com/example/projectmedilog/pHomeController.java
+++ projectMediLog/src/main/java/com/example/projectmedilog/pHomeController.java
@@ -3,12 +3,16 @@ package com.example.projectmedilog;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Button;
 import javafx.scene.layout.Pane;
 
 import java.io.IOException;
 
 
 public class pHomeController {
+    @FXML
+    private Button BTN_prescription;
+
     @FXML
     Pane homePane;
     @FXML
@@ -23,4 +27,24 @@ public class pHomeController {
         Pane DashboardPane = FXMLLoader.load(getClass().getResource("pDashboard.fxml"));
         HomePane2.getChildren().setAll(DashboardPane);
     }
+
+    @FXML
+    void onClickPrescription(ActionEvent event) {
+
+    }
+    @FXML
+    void onCLickMedicalRecords(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onClickSettings(ActionEvent event) {
+
+    }
+    @FXML
+    void onClickLogout(ActionEvent event) {
+
+    }
+
+
 }
