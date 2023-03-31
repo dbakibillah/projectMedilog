c691b97c392740328bdef36364cefd86271de386
Nomanrifat05 <noman.rifat.3@gmail.com>
2023-04-01 02:58:44 +0600

    noman

--- projectMediLog/src/main/java/com/example/projectmedilog/pHomeController.java
+++ projectMediLog/src/main/java/com/example/projectmedilog/pHomeController.java
@@ -34,8 +34,9 @@ public class pHomeController {
     }
 
     @FXML
-    void onClickSettings(ActionEvent event) {
-
+    void onClickSettings(ActionEvent event) throws IOException{
+        Pane SettingPane = FXMLLoader.load(getClass().getResource("Settings.fxml"));
+        HomePane2.getChildren().setAll(SettingPane);
     }
     @FXML
     void onClickLogout(ActionEvent event) {
