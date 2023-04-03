cfc4b5cf746783ffa4fd814d594d402262cd9da6
bakibillah <bakibillah.dev@gmail.com>
2023-04-04 04:03:28 +0600

    image uploading feature added

--- projectMediLog/src/main/java/com/example/projectmedilog/userLogin.java
+++ projectMediLog/src/main/java/com/example/projectmedilog/userLogin.java
@@ -11,17 +11,15 @@ import java.io.IOException;
 import static javafx.application.Application.launch;
 
 public class userLogin extends Application {
-    public void start(Stage stage) throws IOException {
-        //FXMLLoader fxmlLoader = new FXMLLoader(userLogin.class.getResource("userLogin.fxml"));
-        //Scene scene = new Scene(fxmlLoader.load());
-        //stage.setTitle("Hello!");
-        //stage.setScene(scene);
-        //stage.show();
+    static Stage mainstage;
 
+    public void start(Stage stage) throws IOException {
+        mainstage = stage;
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("userLogin.fxml"));
         Parent root = fxmlLoader.load();
         Scene scene = new Scene(root);
         stage.setScene(scene);
+        stage.setTitle("Login");
         stage.show();
     }
 
