4c805ecabb5e17093aa0f0cd41f85703b9766ac5
Nomanrifat05 <noman.rifat.3@gmail.com>
2023-04-08 01:46:59 +0600

    Add Appointment

--- projectMediLog/src/main/java/com/example/projectmedilog/pAppointmentController.java
+++ projectMediLog/src/main/java/com/example/projectmedilog/pAppointmentController.java
@@ -11,9 +11,11 @@ import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.util.StringConverter;
 
+import java.io.IOException;
 import java.net.URL;
-import java.util.ResourceBundle;
 
+import java.util.ResourceBundle;
+import java.sql.*;
 public class pAppointmentController implements Initializable {
 
     @FXML
@@ -67,12 +69,14 @@ public class pAppointmentController implements Initializable {
     @FXML
     private TextField TF_date;
 
+    @FXML
+    private TextField TF_age;
 
     @FXML
     private TextField TF_injury_or_condition;
 
     @FXML
-    private TextField TF_mobile;
+    private TextField TF_phone;
 
     @FXML
     private TextField TF_name;
@@ -97,16 +101,43 @@ public class pAppointmentController implements Initializable {
     }
 
     @FXML
-    void onClickBTN_confirm(ActionEvent event) {
+    void onClickBTN_confirm(ActionEvent event) throws SQLException, ClassNotFoundException {
 
-        String FirstName = TF_name.getText();
-       // String Email = TF_email.getText();
+        String Name = TF_name.getText();
         String Gender = this.Gender;
         String Date = TF_date.getText();
         String Time = CB_time.getValue().toString();
-        String Mobile = TF_mobile.getText();
+        String Phone = TF_phone.getText();
         String Doctor = CB_doctor.getValue().toString();
         String InjuryOrCondition = TF_injury_or_condition.getText();
+        String Age = TF_age.getText();
+
+        //add to database
+
+        Connection connection = database.dbconnect();
+        Statement statement = connection.createStatement();
+
+        try (
+                PreparedStatement pst = connection.prepareStatement("insert into appointment(Name, Email, Gender, Date, Time, Phone, Injury_or_Condition, Doctor, Age) values(?, ?, ?, ?, ?, ?, ?, ?, ?)")
+        ) {
+            pst.setString(1, Name);
+            pst.setString(2, user.getEmail());
+            pst.setString(3, Gender);
+            pst.setString(4, Date);
+            pst.setString(5, Time);
+            pst.setString(6, Phone);
+            pst.setString(7, InjuryOrCondition);
+            pst.setString(8, Doctor);
+            pst.setString(9, Age);
+            pst.executeUpdate();
+
+            System.out.println("Appointment added");
+
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+        //add to table
+
     }
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
@@ -122,7 +153,7 @@ public class pAppointmentController implements Initializable {
             }
         });
 
-        CB_doctor.getItems().addAll();
+        CB_doctor.getItems().addAll("Dr. A", "Dr. B", "Dr. C", "Dr. D", "Dr. E");
         CB_doctor.setConverter(new StringConverter<String>() {
             @Override
             public String toString(String s) {
