5d8e5f5b4e98227ac85637d132d51be61b78da6f
bakibillah <bakibillah.dev@gmail.com>
2023-04-27 01:12:00 +0600

    webview

--- projectMediLog/src/main/java/com/example/projectmedilog/webViewController.java
+++ projectMediLog/src/main/java/com/example/projectmedilog/webViewController.java
@@ -2,7 +2,6 @@ package com.example.projectmedilog;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
@@ -14,23 +13,22 @@ public class webViewController implements Initializable {
     @FXML
     private AnchorPane AnchorPane_webView;
     @FXML
-    private Button BTN;
-    @FXML
     private WebView web_webView;
     private WebEngine webEngine;
 
     private String wpage;
-    public webViewController(String page) {
-        this.wpage = page;
-        System.out.println(page);
-    }
+
+//    public webViewController(String page) {
+//        this.wpage = page;
+//        System.out.println(page);
+//    }
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         webEngine = web_webView.getEngine();
-        loadPage(wpage);
+//        loadPage(wpage);
     }
 
     void loadPage(String page){
-        webEngine.load(page);
+//        webEngine.load(page);
     }
 }
