96a46e95d66b2b65b1524bcf96f57bbf9f149ace
bakibillah <bakibillah.dev@gmail.com>
2023-04-28 21:54:22 +0600

    merge

--- projectMediLog/src/main/java/module-info.java
+++ projectMediLog/src/main/java/module-info.java
@@ -3,13 +3,7 @@ module com.example.projectmedilog {
     requires javafx.fxml;
     requires java.sql;
     requires javafx.media;
-<<<<<<< HEAD
-    requires java.desktop;
-=======
     requires javafx.web;
-
->>>>>>> 2ccb2987851008e8d60db12cc4bfeb0fea5dff6f
-
 
     opens com.example.projectmedilog to javafx.fxml;
     exports com.example.projectmedilog;
